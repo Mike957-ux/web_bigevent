34d91ae8676ecf7ace4212cfa899725d0e48d3bb
liangweihui <3211366346@qq.com>
2022-05-19 11:00:50 +0800

    完成了主页功能

--- assets/js/base.Api.js
+++ assets/js/base.Api.js
@@ -3,8 +3,29 @@
 // 这个函数中我们可以拿到我们给Ajax提供的对象
 $.ajaxPrefilter(
     function (options) {
+
         //发起请求拼接url
         options.url = 'http://www.liulongbin.top:3007' + options.url
-        console.log(options.url);
+        // console.log(options.url);
+
+        // 统一为有权限的接口设置headers请求头
+        if (options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('token') || ''
+            }
+        }
+        // 全局挂载complete回调函数
+        // 无论请求成功还失败都会执行
+        options.complete = function (res) {
+            // console.log(res);
+            // 在complete 回函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 1.清空本地储存的token
+                localStorage.removeItem('token')
+                // 2.跳转到登录页面
+                location.href = '/logoin.html'
+            }
+        }
     }
+
 )
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+// username:mop1 password:000000
+$(function () {
+    // 调用getUserInfo获取用户的信息
+    getUserInfo()
+    var layer = layui.layer
+    // 退出功能
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否退出
+        layer.confirm('确定退出登录？', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 1.清空本地储存的token
+            localStorage.removeItem('token')
+            // 2.跳转到登录页面
+            location.href = '/logoin.html'
+
+            // 关闭
+            layer.close(index);
+        });
+    });
+
+})
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: "/my/userinfo",
+        success: function (res) {
+            // console.log(localStorage.getItem('token'));
+            // console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取失败！！')
+            }
+            // 调用renderAvatar渲染用户头像
+            renderAvatar(res.data)
+        },
+
+    });
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    // 获取用户的名称
+    var name = user.nickname || user.username
+    // 文本信息 用户姓名
+    $('#welcome').html('欢迎，' + name);
+    if (user.user_pic !== null) {
+        // 渲染用户头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide();
+    } else {
+        // 渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
